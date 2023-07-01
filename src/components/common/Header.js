ddaf2d68cd21567e1e3bda5dd65849a1b1bc62ef
unknown <wknowpark797@gmail.com>
2023-07-01 17:48:44 +0900

    Fonts 추가 및 Header 스타일 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,42 +4,49 @@ function Header({ type }) {
 	const activeClass = 'on';
 
 	return (
-		<header className={type}>
-			<h1>
-				<Link to='/'>BOOKS.</Link>
-			</h1>
+		<header id='header' className={type}>
+			<div className='inner-header'>
+				<h1>
+					<Link to='/'>BOOKS.</Link>
+				</h1>
 
-			<ul className='gnb'>
-				<li>
-					<NavLink to='/members' activeClassName={activeClass}>
-						MEMBERS
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/gallery' activeClassName={activeClass}>
-						GALLRY
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube' activeClassName={activeClass}>
-						YOUTUBE
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/comment' activeClassName={activeClass}>
-						COMMENT
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/location' activeClassName={activeClass}>
-						LOCATION
-					</NavLink>
-				</li>
-			</ul>
+				<ul className='gnb'>
+					<li>
+						<NavLink to='/members' activeClassName={activeClass}>
+							MEMBERS
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeClassName={activeClass}>
+							GALLRY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeClassName={activeClass}>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/comment' activeClassName={activeClass}>
+							COMMENT
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/location' activeClassName={activeClass}>
+							LOCATION
+						</NavLink>
+					</li>
+				</ul>
 
-			<div className='login-wrap'>
-				<NavLink to='/'>SIGN IN</NavLink>
-				<NavLink to='/signup'>SIGN UP</NavLink>
+				<div className='login-wrap'>
+					<NavLink to='/'>SIGN IN</NavLink>
+					<NavLink to='/signup'>SIGN UP</NavLink>
+				</div>
+
+				{/* 모바일 메뉴 버튼 */}
+				<button type='button' className='btn-menu'>
+					{/* <i class='fa-solid fa-bars'></i> */}
+				</button>
 			</div>
 		</header>
 	);

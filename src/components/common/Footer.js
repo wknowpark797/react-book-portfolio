5899ca17e1a6d20f8a47a4c062e0f6bc08236f39
unknown <wknowpark797@gmail.com>
2023-07-01 18:28:35 +0900

    Footer 컴포넌트 내용 추가

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,55 @@
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer id='footer'>
+			<div className='inner-footer'>
+				<div className='top'>
+					<h1>
+						<Link to='/'>BOOKS.</Link>
+					</h1>
+
+					<div className='sns-wrap'>
+						<NavLink to=''>
+							<FontAwesomeIcon icon={faFacebook} />
+						</NavLink>
+						<NavLink to=''>
+							<FontAwesomeIcon icon={faTwitter} />
+						</NavLink>
+						<NavLink to=''>
+							<FontAwesomeIcon icon={faInstagram} />
+						</NavLink>
+					</div>
+				</div>
+
+				<div className='bottom'>
+					<ul className='gnb'>
+						<li>
+							<NavLink to='/members'>MEMBERS</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery'>GALLRY</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube'>YOUTUBE</NavLink>
+						</li>
+						<li>
+							<NavLink to='/comment'>COMMENT</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location'>LOCATION</NavLink>
+						</li>
+					</ul>
+
+					<p>
+						&copy; 2023 All rights reserved. <span>Lorem Ipsum Books.</span>
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;

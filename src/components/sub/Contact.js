dfb61deb93f1118640f8072b53a2e6fde3813e0d
unknown <wknowpark797@gmail.com>
2023-07-02 23:20:57 +0900

    Contact.js 컴포넌트 카카오 지도 api 적용, 기본 지도 생성

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,23 +1,98 @@
 import SubLayout from '../common/SubLayout';
 import ContactForm from './ContactForm';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faGlobe, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faYoutube, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
+	const { kakao } = window;
+	const mapContainer = useRef(null);
+	const [Index, setIndex] = useState(0);
+
+	const markerInfo = [
+		{
+			title: '국회도서관',
+			address: '서울특별시 영등포구 의사당대로 1',
+			website: {
+				title: 'nanet.go.kr',
+				link: 'https://www.nanet.go.kr/main.do',
+			},
+			phone: '02-6788-4211',
+			position: new kakao.maps.LatLng(37.531117, 126.917035),
+			imgSrc: `${process.env.PUBLIC_URL}/image/location-pin.png`,
+			imgSize: new kakao.maps.Size(50, 50),
+			imgOption: { offset: new kakao.maps.Point(25, 50) },
+		},
+		{
+			title: '남산도서관',
+			address: '서울특별시 용산구 소월로 109',
+			website: {
+				title: 'nslib.sen.go.kr',
+				link: 'http://nslib.sen.go.kr/nslib_index.jsp',
+			},
+			phone: '02-754-7338',
+			position: new kakao.maps.LatLng(37.552923, 126.981457),
+			imgSrc: `${process.env.PUBLIC_URL}/image/location-pin.png`,
+			imgSize: new kakao.maps.Size(50, 50),
+			imgOption: { offset: new kakao.maps.Point(25, 50) },
+		},
+		{
+			title: '별마당 도서관',
+			address: '서울특별시 강남구 영동대로 513 스타필드 코엑스몰 B1',
+			website: {
+				title: 'starfield.co.kr',
+				link: 'https://www.starfield.co.kr/coexmall/starfieldLibrary/library.do',
+			},
+			phone: '02-6002-3031',
+			position: new kakao.maps.LatLng(37.50999, 127.059986),
+			imgSrc: `${process.env.PUBLIC_URL}/image/location-pin.png`,
+			imgSize: new kakao.maps.Size(50, 50),
+			imgOption: { offset: new kakao.maps.Point(25, 50) },
+		},
+		{
+			title: '국립중앙도서관',
+			address: '서울특별시 서초구 반포대로 201',
+			website: {
+				title: 'nl.go.kr',
+				link: 'https://www.nl.go.kr/',
+			},
+			phone: '02-535-4142',
+			position: new kakao.maps.LatLng(37.497669, 127.002837),
+			imgSrc: `${process.env.PUBLIC_URL}/image/location-pin.png`,
+			imgSize: new kakao.maps.Size(50, 50),
+			imgOption: { offset: new kakao.maps.Point(25, 50) },
+		},
+	];
+
+	const mapOption = { center: markerInfo[Index].position, level: 3 };
+	const imageSrc = markerInfo[Index].imgSrc;
+	const imageSize = markerInfo[Index].imgSize;
+	const imageOption = markerInfo[Index].imgOption;
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+	const marker = new kakao.maps.Marker({ position: mapOption.center, image: markerImage });
+
+	useEffect(() => {
+		const map = new kakao.maps.Map(mapContainer.current, mapOption);
+		marker.setMap(map);
+	}, [Index]);
+
 	return (
 		<SubLayout subPageName={'sub-contact'} breadCrumb={'HOME / CONTACT'} subPageTitle={['WHERE', <br />, 'WE ARE']}>
 			<div className='library-wrap'>
 				<div className='inner-container'>
 					<div className='map-wrap'>
-						<div id='map'></div>
+						<div ref={mapContainer} id='map'></div>
 
 						<ul className='list-branch'>
-							<li className='on'>국회도서관</li>
-							<li>남산도서관</li>
-							<li>별마당 도서관</li>
-							<li>국립중앙도서관</li>
+							{markerInfo.map((item, idx) => {
+								return (
+									<li key={idx} onClick={() => setIndex(idx)} className={Index === idx ? 'on' : ''}>
+										{item.title}
+									</li>
+								);
+							})}
 						</ul>
 
 						<button type='button' className='btn-traffic'>
@@ -27,23 +102,23 @@ function Contact() {
 
 					<div className='library-info'>
 						<div id='infoWrap' className='inner-box'>
-							<h2>국회도서관</h2>
+							<h2>{markerInfo[Index].title}</h2>
 							<ul>
 								<li>
 									<FontAwesomeIcon icon={faLocationDot} />
-									<p>서울특별시 영등포구 의사당대로 1</p>
+									<p>{markerInfo[Index].address}</p>
 								</li>
 								<li>
 									<FontAwesomeIcon icon={faGlobe} />
 									<p>
-										<Link to='https://www.nanet.go.kr/main.do' target='_blank'>
-											nanet.go.kr
+										<Link to={markerInfo[Index].website.link} target='_blank'>
+											{markerInfo[Index].website.title}
 										</Link>
 									</p>
 								</li>
 								<li>
 									<FontAwesomeIcon icon={faPhone} />
-									<p>02-6788-4211</p>
+									<p>{markerInfo[Index].phone}</p>
 								</li>
 							</ul>
 							<div className='sns-wrap'>

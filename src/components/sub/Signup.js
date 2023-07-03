67cf2546239e42014ef4089d7bacdfe74624ccc3
unknown <wknowpark797@gmail.com>
2023-07-03 18:40:12 +0900

    Signup.js 컴포넌트 내용 추가

--- src/components/sub/Signup.js
+++ src/components/sub/Signup.js
@@ -1,5 +1,169 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faOptinMonster, faEarlybirds, faFacebookF, faGooglePlusG, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 function Signup() {
-	return <div>Signup</div>;
+	return (
+		<section className='sign-up'>
+			<div className='inner-container'>
+				<div className='desc-wrap'>
+					<div className='title-wrap'>
+						<h2>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+						<p>
+							<span>DETAIL DESCRIPTION</span>
+						</p>
+					</div>
+
+					<div className='change-wrap'>
+						<ul className='guideListWrap'>
+							<li>회원가입을 위한 입력 항목입니다.</li>
+							<li>입력 항목에 커서를 올리면 안내사항이 표시됩니다.</li>
+						</ul>
+					</div>
+
+					<ul className='list-notice'>
+						<li>
+							<FontAwesomeIcon icon={faOptinMonster} />
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem dolores perferendis asperiores.</p>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faEarlybirds} />
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem dolores perferendis asperiores.</p>
+						</li>
+					</ul>
+				</div>
+
+				<div className='signup-box'>
+					<div className='top-wrap'>
+						<h1>Create Account</h1>
+
+						<div className='sns-wrap'>
+							<Link to='#'>
+								<FontAwesomeIcon icon={faFacebookF} />
+							</Link>
+							<Link to='#'>
+								<FontAwesomeIcon icon={faGooglePlusG} />
+							</Link>
+							<Link to='#'>
+								<FontAwesomeIcon icon={faLinkedinIn} />
+							</Link>
+						</div>
+
+						<p>or use your email for registration:</p>
+					</div>
+
+					<form id='formSignup' className='form-signup'>
+						<fieldset>
+							<legend className='h'>회원가입 form 입력 항목</legend>
+
+							{/* user id */}
+							<div className='input-box'>
+								<label htmlFor='userid' className='tit'>
+									User ID
+								</label>
+								<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요.' />
+							</div>
+
+							{/* password */}
+							<div className='input-box'>
+								<label htmlFor='pwd1' className='tit'>
+									Password
+								</label>
+								<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요.' />
+							</div>
+
+							<div className='input-box'>
+								<label htmlFor='pwd2' className='tit'>
+									Re Passsword
+								</label>
+								<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재 입력하세요.' />
+							</div>
+
+							{/* Email */}
+							<div className='input-box'>
+								<label htmlFor='email' className='tit'>
+									E-mail
+								</label>
+								<input type='email' name='email' id='email' placeholder='이메일 주소를 입력하세요.' />
+							</div>
+
+							{/* Education */}
+							<div className='input-box'>
+								<label htmlFor='edu' className='tit'>
+									Education
+								</label>
+
+								<select name='edu' id='edu'>
+									<option value=''>최종학력을 선택해주세요.</option>
+									<option value='elementary school'>초등학교 졸업</option>
+									<option value='middle school'>중학교 졸업</option>
+									<option value='high school'>고등학교 졸업</option>
+									<option value='college'>대학교 졸업</option>
+								</select>
+							</div>
+
+							{/* Gender */}
+							<div className='input-box'>
+								<p className='tit'>Gender</p>
+
+								<div className='radio-wrap'>
+									<div>
+										<input type='radio' name='gender' id='male' value='male' />
+										<label htmlFor='male'>Male</label>
+									</div>
+
+									<div>
+										<input type='radio' name='gender' id='female' value='female' />
+										<label htmlFor='female'>Female</label>
+									</div>
+								</div>
+							</div>
+
+							{/* Interests */}
+							<div className='input-box'>
+								<p className='tit'>Interests</p>
+
+								<div className='check-wrap'>
+									<div>
+										<input type='checkbox' name='hobby' id='sports' value='sports' />
+										<label htmlFor='sports'>Sports</label>
+									</div>
+
+									<div>
+										<input type='checkbox' name='hobby' id='music' value='music' />
+										<label htmlFor='music'>Music</label>
+									</div>
+
+									<div>
+										<input type='checkbox' name='hobby' id='game' value='game' />
+										<label htmlFor='game'>Game</label>
+									</div>
+
+									<div>
+										<input type='checkbox' name='hobby' id='reading' value='reading' />
+										<label htmlFor='reading'>Reading</label>
+									</div>
+								</div>
+							</div>
+
+							{/* Comments */}
+							<div className='input-box'>
+								<label htmlFor='comments' className='tit'>
+									Comments
+								</label>
+								<textarea name='comments' id='comments' cols='30' rows='7' placeholder='남기는 글을 입력하세요.'></textarea>
+							</div>
+
+							<div className='btn-wrap'>
+								<input type='reset' value='RESET' />
+								<input type='submit' value='SIGN UP' />
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Signup;

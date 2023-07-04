63fbd9114e95e1581cac41b6951d904768413d50
unknown <wknowpark797@gmail.com>
2023-07-04 23:17:08 +0900

    Reviews.js 데이터 Fetch 및 Create 기능 구현

--- src/components/sub/Review.js
+++ src/components/sub/Review.js
@@ -1,6 +1,54 @@
 import SubLayout from '../common/SubLayout';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
 function Review() {
+	const inputBookName = useRef(null);
+	const inputReviewContent = useRef(null);
+	const [Reviews, setReviews] = useState([]);
+
+	useEffect(() => {
+		dataFetch();
+	}, []);
+
+	const dataFetch = async () => {
+		const result = await axios.get(`${process.env.PUBLIC_URL}/DB/reviews.json`);
+		setReviews(result.data.reviews);
+	};
+
+	const setToday = () => {
+		const today = new Date();
+		const year = today.getFullYear();
+		const month = today.getMonth() < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
+		const date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
+
+		return `${year}.${month}.${date}`;
+	};
+
+	const resetForm = () => {
+		inputBookName.current.value = '';
+		inputReviewContent.current.value = '';
+	};
+
+	const createReview = () => {
+		if (!inputBookName.current.value.trim() || !inputReviewContent.current.value.trim()) {
+			resetForm();
+			return alert('도서명과 리뷰 내용을 모두 입력하세요.');
+		}
+
+		setReviews([
+			{
+				bookName: inputBookName.current.value,
+				reviewContent: inputReviewContent.current.value,
+				profileImg: 'my-profile.jpg',
+				userName: 'Woo Ara',
+				date: setToday(),
+			},
+			...Reviews,
+		]);
+		resetForm();
+	};
+
 	return (
 		<SubLayout subPageName={'sub-review'} breadCrumb={'HOME / REVIEW'} subPageTitle={['EXPERIENCES', <br />, 'FOR BOOK']}>
 			<div className='review-wrap'>
@@ -11,19 +59,21 @@ function Review() {
 							<label htmlFor='bookname' className='tit'>
 								Book Name
 							</label>
-							<input type='text' id='bookname' placeholder='도서명을 입력하세요.' />
+							<input type='text' id='bookname' placeholder='도서명을 입력하세요.' ref={inputBookName} />
 						</div>
 
 						<div className='input-box'>
 							<label htmlFor='reviewcontent' className='tit'>
 								Review Content
 							</label>
-							<textarea id='reviewcontent' placeholder='리뷰 내용을 작성해주세요.'></textarea>
+							<textarea id='reviewcontent' placeholder='리뷰 내용을 작성해주세요.' ref={inputReviewContent}></textarea>
 						</div>
 
 						<div className='btn-wrap'>
-							<button type='button'>RESET</button>
-							<button type='button' className='btn-write'>
+							<button type='button' onClick={resetForm}>
+								RESET
+							</button>
+							<button type='button' className='btn-write' onClick={createReview}>
 								WRITE
 							</button>
 						</div>
@@ -31,30 +81,34 @@ function Review() {
 
 					{/* review list */}
 					<div className='show-wrap'>
-						<article>
-							<h2>일상의 빈칸</h2>
+						{Reviews.map((review, idx) => {
+							return (
+								<article key={idx}>
+									<h2>{review.bookName}</h2>
+									<p>{review.reviewContent}</p>
 
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet accusamus sunt illo amet mollitia numquam quidem dolor. Fugit, tempora optio.</p>
+									<div className='info-wrap'>
+										<div className='profile-box'>
+											<img src={`${process.env.PUBLIC_URL}/image/${review.profileImg}`} alt='' />
+										</div>
+										<div className='info-box'>
+											<p className='user'>{review.userName}</p>
+											<p>{review.date}</p>
+										</div>
+									</div>
 
-							<div className='info-wrap'>
-								<div className='profile-box'>
-									<img src='' alt='' />
-								</div>
-
-								<div className='info-box'>
-									<p className='user'>작성자</p>
-									<p>2023.07.04</p>
-								</div>
-							</div>
-
-							{/* 나의 게시물에서만 노출 */}
-							<div className='btn-wrap'>
-								<button type='button'>EDIT</button>
-								<button type='button' className='btn-delete'>
-									DELETE
-								</button>
-							</div>
-						</article>
+									{/* 내가 작성한 게시물에서만 노출 */}
+									{review.userName === 'Woo Ara' && (
+										<div className='btn-wrap'>
+											<button type='button'>EDIT</button>
+											<button type='button' className='btn-delete'>
+												DELETE
+											</button>
+										</div>
+									)}
+								</article>
+							);
+						})}
 					</div>
 				</div>
 			</div>

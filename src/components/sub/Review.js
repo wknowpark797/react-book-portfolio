4f96daaf7115e87700371860d0dcd89707d4915d
unknown <wknowpark797@gmail.com>
2023-07-04 19:06:31 +0900

    Review.js 화면 완료

--- src/components/sub/Review.js
+++ src/components/sub/Review.js
@@ -5,8 +5,8 @@ function Review() {
 		<SubLayout subPageName={'sub-review'} breadCrumb={'HOME / REVIEW'} subPageTitle={['EXPERIENCES', <br />, 'FOR BOOK']}>
 			<div className='review-wrap'>
 				<div className='inner-container'>
+					{/* review input */}
 					<div className='input-wrap'>
-						{/* book name */}
 						<div className='input-box'>
 							<label htmlFor='bookname' className='tit'>
 								Book Name
@@ -14,12 +14,11 @@ function Review() {
 							<input type='text' id='bookname' placeholder='도서명을 입력하세요.' />
 						</div>
 
-						{/* review content */}
 						<div className='input-box'>
-							<label htmlFor='bookreview' className='tit'>
+							<label htmlFor='reviewcontent' className='tit'>
 								Review Content
 							</label>
-							<textarea id='bookreview' placeholder='리뷰 내용을 작성해주세요.'></textarea>
+							<textarea id='reviewcontent' placeholder='리뷰 내용을 작성해주세요.'></textarea>
 						</div>
 
 						<div className='btn-wrap'>
@@ -30,44 +29,30 @@ function Review() {
 						</div>
 					</div>
 
+					{/* review list */}
 					<div className='show-wrap'>
 						<article>
 							<h2>일상의 빈칸</h2>
 
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet accusamus sunt illo amet mollitia numquam quidem dolor. Fugit, tempora optio.</p>
 
-							<div className='info-box'>
-								<div class='profile-box'>
+							<div className='info-wrap'>
+								<div className='profile-box'>
 									<img src='' alt='' />
 								</div>
 
-								<div>
+								<div className='info-box'>
 									<p className='user'>작성자</p>
 									<p>2023.07.04</p>
 								</div>
 							</div>
 
+							{/* 나의 게시물에서만 노출 */}
 							<div className='btn-wrap'>
 								<button type='button'>EDIT</button>
 								<button type='button' className='btn-delete'>
 									DELETE
 								</button>
-							</div>
-						</article>
-						<article>
-							<h2>일상의 빈칸</h2>
-
-							<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur mollitia suscipit illo eos reprehenderit repudiandae.</p>
-
-							<div className='info-box'>
-								<div class='profile-box'>
-									<img src='' alt='' />
-								</div>
-
-								<div>
-									<p className='user'>작성자</p>
-									<p>2023.07.04</p>
-								</div>
 							</div>
 						</article>
 					</div>

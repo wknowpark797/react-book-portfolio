b24cca279e7daca47e547c248730b0f6ac4f12b2
unknown <wknowpark797@gmail.com>
2023-07-08 23:58:06 +0900

    Modal.js 모션 라이브러리 설치 및 적용

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,6 +1,7 @@
-import { useState, forwardRef, useImperativeHandle } from 'react';
+import { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Modal = forwardRef((props, ref) => {
 	const [IsOpen, setIsOpen] = useState(false);
@@ -8,20 +9,41 @@ const Modal = forwardRef((props, ref) => {
 		return { open: () => setIsOpen(true) };
 	});
 
+	useEffect(() => {
+		IsOpen ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = 'auto');
+	}, [IsOpen]);
+
 	return (
-		<>
+		<AnimatePresence>
 			{IsOpen && (
-				<aside ref={ref} className='pop-wrap'>
-					<div className='inner-pop'>
+				<motion.aside
+					className='pop-wrap'
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1, transition: { duration: 0.2 } }}
+					exit={{ opacity: 0, transition: { duration: 0.2 } }}
+				>
+					<motion.div
+						className='inner-pop'
+						initial={{ opacity: 0, y: 100 }}
+						animate={{ opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.2 } }}
+						exit={{ opacity: 0, y: 100, transition: { duration: 0.3, delay: 0 } }}
+					>
 						<div className='inner-content'>{props.children}</div>
 
-						<button type='button' className='pop-close' onClick={() => setIsOpen(false)}>
+						<motion.button
+							type='button'
+							className='pop-close'
+							onClick={() => setIsOpen(false)}
+							initial={{ scale: 0 }}
+							animate={{ scale: 1, transition: { duration: 0.2, delay: 0.5 } }}
+							exit={{ scale: 0, transition: { duration: 0.2, delay: 0 } }}
+						>
 							<FontAwesomeIcon icon={faCircleXmark} />
-						</button>
-					</div>
-				</aside>
+						</motion.button>
+					</motion.div>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 });
 

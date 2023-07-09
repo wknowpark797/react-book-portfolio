5eb24bc2febd0e54ec19e9490ae9772180bdf95c
unknown <wknowpark797@gmail.com>
2023-07-09 22:26:35 +0900

    BookList.js 화면 작업

--- src/components/main/BookList.js
+++ src/components/main/BookList.js
@@ -1,5 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 function BookList() {
-	return <div>BookList</div>;
+	return (
+		<section id='main-book-list' className='my-scroll'>
+			<div className='inner-container'>
+				<div className='title-wrap'>
+					<h1>Best Seller Books.</h1>
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, exercitationem.</p>
+
+					<div className='arrow'>
+						<button type='button' id='btnPrevBook' className='prev'>
+							<FontAwesomeIcon icon={faChevronLeft} />
+						</button>
+						<button type='button' id='btnNextBook' className='next'>
+							<FontAwesomeIcon icon={faChevronRight} />
+						</button>
+					</div>
+				</div>
+
+				<div className='slide-wrap bookListSwiper'>
+					<article id='bookListPanel' className='panel swiper-wrapper'>
+						<div className='swiper-slide'>
+							<div className='ratio-wrap'>
+								<div className='img-box'>
+									<img src='img/example_book4.jpg' alt='' />
+								</div>
+							</div>
+
+							<div className='info-box'>
+								<h2>Book Name</h2>
+								<p>William H. Brock</p>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default BookList;

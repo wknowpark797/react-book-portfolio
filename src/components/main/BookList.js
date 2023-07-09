61f30731c0f39fb06e8dc9b3855143509e728c11
unknown <wknowpark797@gmail.com>
2023-07-09 22:46:13 +0900

    BookList.js Swiper 적용 및 데이터 가져오기

--- src/components/main/BookList.js
+++ src/components/main/BookList.js
@@ -1,7 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 
 function BookList() {
+	const btnPrev = useRef(null);
+	const btnNext = useRef(null);
+	const [Items, setItems] = useState([]);
+
+	const getBookData = async () => {
+		const userId = '105834502729522452212';
+		const shelf = '1001';
+		const listURL = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelf}/volumes?maxResults=30`;
+
+		const result = await axios.get(listURL);
+		setItems(result.data.items);
+	};
+
+	useEffect(() => {
+		getBookData();
+	}, []);
+
 	return (
 		<section id='main-book-list' className='my-scroll'>
 			<div className='inner-container'>
@@ -10,30 +32,41 @@ function BookList() {
 					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, exercitationem.</p>
 
 					<div className='arrow'>
-						<button type='button' id='btnPrevBook' className='prev'>
+						<button type='button' id='btnPrevBook' className='prev' ref={btnPrev}>
 							<FontAwesomeIcon icon={faChevronLeft} />
 						</button>
-						<button type='button' id='btnNextBook' className='next'>
+						<button type='button' id='btnNextBook' className='next' ref={btnNext}>
 							<FontAwesomeIcon icon={faChevronRight} />
 						</button>
 					</div>
 				</div>
 
 				<div className='slide-wrap bookListSwiper'>
-					<article id='bookListPanel' className='panel swiper-wrapper'>
-						<div className='swiper-slide'>
-							<div className='ratio-wrap'>
-								<div className='img-box'>
-									<img src='img/example_book4.jpg' alt='' />
-								</div>
-							</div>
+					<Swiper
+						id='bookListPanel'
+						className='panel'
+						slidesPerView={'auto'}
+						spaceBetween={30}
+						navigation={{ nextEl: btnNext.current, prevEl: btnPrev.current }}
+						modules={[Navigation]}
+					>
+						{Items.map((item, idx) => {
+							return (
+								<SwiperSlide key={idx}>
+									<div className='ratio-wrap'>
+										<div className='img-box'>
+											<img src={item.volumeInfo.imageLinks.thumbnail.replace('zoom=1', 'zoom=10')} alt='' />
+										</div>
+									</div>
 
-							<div className='info-box'>
-								<h2>Book Name</h2>
-								<p>William H. Brock</p>
-							</div>
-						</div>
-					</article>
+									<div className='info-box'>
+										<h2>{item.volumeInfo.title}</h2>
+										<p>{item.volumeInfo.authors}</p>
+									</div>
+								</SwiperSlide>
+							);
+						})}
+					</Swiper>
 				</div>
 			</div>
 		</section>

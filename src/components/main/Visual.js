a47554e26c203ea00a5130623de3d28bd9fc8705
unknown <wknowpark797@gmail.com>
2023-07-09 18:45:58 +0900

    Visual.js Swiper 적용 및 데이터 연동 작업중

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function Visual() {
+	const [Items, setItems] = useState([]);
+
+	const getBookData = async () => {
+		const userId = '105834502729522452212';
+		const shelf = '1002';
+		const listURL = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelf}/volumes?maxResults=30`;
+
+		const result = await axios.get(listURL);
+		setItems(result.data.items);
+	};
+
+	useEffect(() => {
+		getBookData();
+	}, []);
+
 	return (
 		<section id='main-visual' className='my-scroll on'>
 			<div className='bg-wrap'>
@@ -12,55 +31,61 @@ function Visual() {
 			</div>
 
 			<div className='slide-wrap mainVisualSwiper'>
-				<article id='visualPanel' className='panel swiper-wrapper'>
-					<div className='swiper-slide'>
-						<div className='detail-wrap'>
-							<h1>The Case of the Poisonous Socks</h1>
+				<Swiper id='visualPanel' className='panel'>
+					{Items.map((item, idx) => {
+						return (
+							<SwiperSlide key={idx}>
+								<div className='detail-wrap'>
+									<h1>${item.volumeInfo.title}</h1>
 
-							<div className='rating-wrap'>
-								<div className='star-box'>
-									<span className='on'>
-										<FontAwesomeIcon icon={faStar} />
-									</span>
-									<span className='on'>
-										<FontAwesomeIcon icon={faStar} />
-									</span>
-									<span className='on'>
-										<FontAwesomeIcon icon={faStar} />
-									</span>
-									<span>
-										<FontAwesomeIcon icon={faStar} />
-									</span>
-									<span>
-										<FontAwesomeIcon icon={faStar} />
-									</span>
-								</div>
-								<p>3.0</p>
-							</div>
+									<div className='rating-wrap'>
+										<div className='star-box'>
+											<span className='on'>
+												<FontAwesomeIcon icon={faStar} />
+											</span>
+											<span className='on'>
+												<FontAwesomeIcon icon={faStar} />
+											</span>
+											<span className='on'>
+												<FontAwesomeIcon icon={faStar} />
+											</span>
+											<span>
+												<FontAwesomeIcon icon={faStar} />
+											</span>
+											<span>
+												<FontAwesomeIcon icon={faStar} />
+											</span>
+										</div>
+										<p>3.0</p>
+									</div>
 
-							<p className='content'>
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatem deleniti velit nostrum
-								consequuntur in consectetur deserunt vitae libero maiores.
-							</p>
+									<p className='content'>${item.volumeInfo.description}</p>
 
-							<button type='button' className='btn-more'>
-								VIEW DETAIL
-							</button>
-						</div>
-
-						<div className='ratio-wrap'>
-							<div className='double-wrap'>
-								<div className='img-box origin'>
-									<img src='img/example_book1.jpg' alt='' />
+									<button type='button' className='btn-more'>
+										VIEW DETAIL
+									</button>
 								</div>
 
-								<div className='img-box shadow'>
-									<img src='img/example_book1.jpg' alt='' />
+								<div className='ratio-wrap'>
+									<div className='double-wrap'>
+										<div className='img-box origin'>
+											<img
+												src={item.volumeInfo.imageLinks.thumbnail
+													.replace('zoom=1', 'zoom=10')
+													.replace('edge=curl', 'edge=')}
+												alt=''
+											/>
+										</div>
+
+										<div className='img-box shadow'>
+											<img src={item.volumeInfo.imageLinks.thumbnail.replace('edge=curl', 'edge=')} alt='' />
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
-					</div>
-				</article>
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
 
 				<div className='indicator'>
 					<div className='current-number'>

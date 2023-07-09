07ec8ae95f01d460bac8b9b34fd850ee5fefa4d0
unknown <wknowpark797@gmail.com>
2023-07-09 23:02:46 +0900

    MainYoutube.js 화면 작업

--- src/components/main/MainYoutube.js
+++ src/components/main/MainYoutube.js
@@ -1,5 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons';
+
 function MainYoutube() {
-	return <div>MainYoutube</div>;
+	return (
+		<section id='main-youtube-list' className='my-scroll'>
+			<div className='inner-container'>
+				<div className='title-wrap'>
+					<h1>
+						Youtube <br />
+						Discover The World.
+					</h1>
+					<a href='youtube.html' className='btn-video'>
+						<FontAwesomeIcon icon={faCirclePlay} />
+						<span>WATCH THE VIDEO</span>
+					</a>
+				</div>
+
+				<div className='inner-content'>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum non atque ex assumenda blanditiis modi
+						numquam dolores hic quos veritatis?
+					</p>
+
+					<div id='mainYoutubeWrap' className='video-wrap'>
+						<article>
+							<img src='' alt='' />
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default MainYoutube;

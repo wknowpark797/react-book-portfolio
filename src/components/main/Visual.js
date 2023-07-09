01f308e06234faa93eec3952f2e80808205754b3
unknown <wknowpark797@gmail.com>
2023-07-09 22:19:44 +0900

    Visual.js Swiper 기능 추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,12 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 
 function Visual() {
+	const btnPrev = useRef(null);
+	const btnNext = useRef(null);
+	const fractionFrame = useRef(null);
 	const [Items, setItems] = useState([]);
+	const ratingList = [5, 4, 3, 4, 5];
 
 	const getBookData = async () => {
 		const userId = '105834502729522452212';
@@ -31,35 +36,50 @@ function Visual() {
 			</div>
 
 			<div className='slide-wrap mainVisualSwiper'>
-				<Swiper id='visualPanel' className='panel'>
+				<Swiper
+					id='visualPanel'
+					className='panel'
+					pagination={{
+						el: fractionFrame.current,
+						type: 'fraction',
+						formatFractionCurrent: (num) => {
+							return num < 10 && (num = '0' + num);
+						},
+						formatFractionTotal: (num) => {
+							return num < 10 && (num = '0' + num);
+						},
+					}}
+					navigation={{ nextEl: btnNext.current, prevEl: btnPrev.current }}
+					modules={[Pagination, Navigation]}
+				>
 					{Items.map((item, idx) => {
 						return (
 							<SwiperSlide key={idx}>
 								<div className='detail-wrap'>
-									<h1>${item.volumeInfo.title}</h1>
+									<h1>{item.volumeInfo.title}</h1>
 
 									<div className='rating-wrap'>
 										<div className='star-box'>
-											<span className='on'>
+											<span className={ratingList[idx] >= 1 ? 'on' : ''}>
 												<FontAwesomeIcon icon={faStar} />
 											</span>
-											<span className='on'>
+											<span className={ratingList[idx] >= 2 ? 'on' : ''}>
 												<FontAwesomeIcon icon={faStar} />
 											</span>
-											<span className='on'>
+											<span className={ratingList[idx] >= 3 ? 'on' : ''}>
 												<FontAwesomeIcon icon={faStar} />
 											</span>
-											<span>
+											<span className={ratingList[idx] >= 4 ? 'on' : ''}>
 												<FontAwesomeIcon icon={faStar} />
 											</span>
-											<span>
+											<span className={ratingList[idx] >= 5 ? 'on' : ''}>
 												<FontAwesomeIcon icon={faStar} />
 											</span>
 										</div>
 										<p>3.0</p>
 									</div>
 
-									<p className='content'>${item.volumeInfo.description}</p>
+									<p className='content'>{item.volumeInfo.description}</p>
 
 									<button type='button' className='btn-more'>
 										VIEW DETAIL
@@ -88,15 +108,15 @@ function Visual() {
 				</Swiper>
 
 				<div className='indicator'>
-					<div className='current-number'>
+					<div className='current-number' ref={fractionFrame}>
 						<span>01</span> / 05
 					</div>
 
 					<div className='btn-arrow'>
-						<button type='button' id='btnPrevVisual'>
+						<button type='button' id='btnPrevVisual' ref={btnPrev}>
 							<FontAwesomeIcon icon={faArrowLeft} />
 						</button>
-						<button type='button' id='btnNextVisual'>
+						<button type='button' id='btnNextVisual' ref={btnNext}>
 							<FontAwesomeIcon icon={faArrowRight} />
 						</button>
 					</div>

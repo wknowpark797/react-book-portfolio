bf8a39876c34e6e1d44243c8dd0ead3ac640a84e
unknown <wknowpark797@gmail.com>
2023-07-15 16:17:02 +0900

    main 페이지 Scroll Navi 화면 추가

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,6 +5,7 @@ import MainGallery from './MainGallery';
 import MainYoutube from './MainYoutube';
 import MainComment from './MainComment';
 import MainLocation from './MainLocation';
+import ScrollNavi from './ScrollNavi';
 
 function Main() {
 	return (
@@ -17,6 +18,8 @@ function Main() {
 			<MainYoutube />
 			<MainComment />
 			<MainLocation />
+
+			<ScrollNavi />
 		</main>
 	);
 }
---
+++ src/components/main/ScrollNavi.js
@@ -0,0 +1,14 @@
+function ScrollNavi() {
+	return (
+		<ul class='scroll-navi'>
+			<li class='on'></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	);
+}
+
+export default ScrollNavi;

4acb91ff845ea48c6a32844aa3869dae565379e7
wknowpark797 <wknowpark797@gmail.com>
2023-07-28 11:59:59 +0900

    로그인 화면 내용 추가 및 스타일 적용

--- src/components/sub/Signin.js
+++ src/components/sub/Signin.js
@@ -1,5 +1,42 @@
 function Signin() {
-	return <div>로그인 페이지</div>;
+	return (
+		<section className='sign-in'>
+			<div className='inner-container'>
+				<div className='signin-box'>
+					<div className='top-wrap'>
+						<h1>Sign In</h1>
+					</div>
+
+					<form id='formSignin' className='form-signin'>
+						<fieldset>
+							<legend className='h'>로그인 form 입력 항목</legend>
+
+							{/* Email */}
+							<div className='input-box'>
+								<label htmlFor='email' className='tit'>
+									E-mail
+								</label>
+								<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요.' />
+							</div>
+
+							{/* password */}
+							<div className='input-box'>
+								<label htmlFor='pwd' className='tit'>
+									Password
+								</label>
+								<input type='password' name='pwd' id='pwd' placeholder='비밀번호를 입력하세요.' />
+							</div>
+
+							<div className='btn-wrap'>
+								<input type='submit' value='SIGN IN' />
+								<button>SIGN UP</button>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Signin;
